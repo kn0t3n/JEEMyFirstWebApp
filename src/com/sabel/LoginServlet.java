40ca4ba933cb6edf6748eaa7a9948133b574ccec
knoten85 <philipp.schweiger85@googlemail.com>
2017-12-15 08:39:42 +0100

    init

---
+++ src/com/sabel/LoginServlet.java
@@ -0,0 +1,29 @@
+package com.sabel;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/login.do")
+
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+        writer.println("<head>");
+        writer.println("<title>My First WebApp</title>");
+        writer.println("</head>");
+        writer.println("<body>");
+        writer.println("<h1>Meine Erste WebApp</h1>");
+        writer.println("<p>Hallo Spacko</p>");
+        writer.println("</body>");
+        writer.println("</html>");
+
+    }
+}
