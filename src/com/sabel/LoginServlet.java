e1ef4f9c63cfd17c817e4f25e4929734367de72a
knoten85 <philipp.schweiger85@googlemail.com>
2017-12-15 09:18:36 +0100

    step3: welcome.jsp, form with post

--- src/com/sabel/LoginServlet.java
+++ src/com/sabel/LoginServlet.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/login.do")
 
@@ -14,7 +13,12 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("name", req.getParameter("name"));
         req.getRequestDispatcher("WEB-INF/Views/login.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("name", req.getParameter("name"));
+        req.getRequestDispatcher("/WEB-INF/Views/welcome.jsp").forward(req, resp);
     }
 }
--- web/WEB-INF/Views/welcome.jsp
+++ web/WEB-INF/Views/welcome.jsp
@@ -2,18 +2,15 @@
   Created by IntelliJ IDEA.
   User: Philipp Schweiger
   Date: 15.12.2017
-  Time: 08:43
+  Time: 09:14
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Meine erste JSP</title>
+    <title>Willkommensseite</title>
 </head>
 <body>
-<form action="/login.do" method="post">
-    Name: <input type="text" name="name"/> Passwort: <input type="password" name="password"/>
-    <input type="submit" value="Anmelden">
-</form>
+<p>Hallo ${name}! Herzlich Willkomen</p>
 </body>
 </html>
